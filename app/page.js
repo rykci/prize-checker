106e2bfb141988aa8ff88a9734c6cf4990f16527
rykci <yuen.ricky@hotmail.com>
2025-02-24 12:15:53 -0500

    prize checker v1

--- app/page.js
+++ app/page.js
@@ -1,101 +1,161 @@
-import Image from "next/image";
+'use client'
+import Image from 'next/image'
+import { Textarea } from '@mantine/core'
+import { Button } from '@mantine/core'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const cardBack = {
+    img: '/card-back.png',
+    prizeDrawn: false,
+  }
+  let [decklist, setDecklist] = useState('')
+  let [deckInfo, setDeckInfo] = useState([])
+  let [prizes, setPrizes] = useState([
+    cardBack,
+    cardBack,
+    cardBack,
+    cardBack,
+    cardBack,
+    cardBack,
+  ])
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  const resetImport = () => {
+    setDeckInfo([])
+    setPrizes([cardBack, cardBack, cardBack, cardBack, cardBack, cardBack])
+  }
+
+  const handleImport = () => {
+    let rows = decklist.split('\n')
+
+    rows = rows.filter((str) => /^[0-9]/.test(str))
+
+    let deckImages = rows.map((row, indexInDeck) => {
+      let words = row.split(' ')
+      let quantity = words[0]
+      let cardNumber = words[words.length - 1]
+      let setSymbol = words[words.length - 2]
+
+      cardNumber = cardNumber.padStart(3, '0')
+      let img = `https://limitlesstcg.nyc3.digitaloceanspaces.com/tpci/${setSymbol}/${setSymbol}_${cardNumber}_R_EN.png`
+      return {
+        quantity,
+        setSymbol,
+        cardNumber,
+        img,
+        indexInDeck,
+      }
+    })
+
+    setDeckInfo(deckImages)
+    setPrizes([cardBack, cardBack, cardBack, cardBack, cardBack, cardBack])
+  }
+
+  const handlePrize = (e, cardIndex) => {
+    // e.preventDefault()
+    let card = deckInfo[cardIndex]
+    let newDeckInfo = [...deckInfo]
+    let newPrizes = prizes.filter((p) => p.img != cardBack.img)
+    if (card.quantity > 0 && newPrizes.length < 6) {
+      card.quantity -= 1
+
+      newDeckInfo[cardIndex] = card
+      newPrizes.push({ ...card, prizeDrawn: false })
+
+      while (newPrizes.length < 6) {
+        newPrizes.push(cardBack)
+      }
+
+      console.log(newPrizes)
+      setDeckInfo(newDeckInfo)
+      setPrizes(newPrizes)
+    }
+  }
+
+  const handleRemovePrize = (index) => {
+    if (prizes[index].img == cardBack.img) {
+    } else {
+      let newPrizes = [...prizes]
+      let newDeckInfo = [...deckInfo]
+
+      if (newPrizes[index].prizeDrawn) {
+        let removedCard = newPrizes.splice(index, 1).pop()
+        newDeckInfo[removedCard.indexInDeck].quantity++
+        newPrizes.push(cardBack)
+      } else {
+        newPrizes[index].prizeDrawn = true
+      }
+
+      setDeckInfo(newDeckInfo)
+      setPrizes(newPrizes)
+    }
+  }
+
+  return (
+    <div className='flex flex-col min-h-screen font-[family-name:var(--font-geist-sans)]'>
+      <main className='grow flex flex-col  items-center  min-w-screen'>
+        <div>Manual Prize Checker</div>
+        {deckInfo.length == 0 ? (
+          <div className='grow flex flex-col justify-center w-2/5'>
+            <Textarea
+              className='pb-10 w-auto'
+              variant='filled'
+              placeholder='Enter decklist'
+              autosize
+              minRows={2}
+              maxRows={4}
+              value={decklist}
+              onChange={(e) => setDecklist(e.target.value)}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <Button onClick={handleImport} className='w-screen p-16'>
+              Import
+            </Button>
+          </div>
+        ) : (
+          <Button onClick={resetImport} className='w-screen p-16'>
+            Reset
+          </Button>
+        )}
+        <div className='deck sm:w-fit md:w-screen lg:w-1/2 grid grid-cols-3 gap-1 lg:p-16'>
+          {deckInfo.length > 0 ? (
+            prizes.map((card, index) => (
+              <div
+                key={index}
+                className={`relative ${
+                  card.img != cardBack.img ? 'hover:cursor-pointer' : ''
+                } ${card.prizeDrawn ? 'grayscale' : ''}`}
+                onClick={() => handleRemovePrize(index)}
+              >
+                <img src={card.img} />
+              </div>
+            ))
+          ) : (
+            <></>
+          )}
+        </div>
+        <div className='deck sm:w-fit md:w-screen lg:w-1/2 grid grid-cols-8 gap-1'>
+          {deckInfo.length > 0 ? (
+            deckInfo.map((card) => (
+              <div
+                onClick={(e) => handlePrize(e, card.indexInDeck)}
+                key={card.indexInDeck}
+                className={`relative ${
+                  card.quantity > 0 ? 'hover:cursor-pointer' : 'grayscale'
+                }`}
+              >
+                <img src={card.img} />
+                <div className='absolute left-1/2 transform -translate-x-1/2 bottom-0 text-white font-semibold bg-red-800 border-2 rounded-full sm:w-8 sm:h-8 lg:w-8 lg:h-8 text-center flex items-center justify-center'>
+                  {card.quantity}
+                </div>
+              </div>
+            ))
+          ) : (
+            <></>
+          )}
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <footer className=''>by rykci</footer>
     </div>
-  );
+  )
 }
